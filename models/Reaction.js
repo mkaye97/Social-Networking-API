0d30ea490c52c474e666cd426d4c958bfbef255c
mkaye <mattkaye97@gmail.com>
2023-04-24 00:02:03 -0400

    Defined Reaction model. Added all models to index.js requirements.

---
+++ models/Reaction.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Thought model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return `${this.reactions.length}`;
+  });
+
+// Initialize our Reaction model
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
