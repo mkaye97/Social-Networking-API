c814a5b217dd5b73e8b0a0d1d4f30004da2acb68
mkaye <mattkaye97@gmail.com>
2023-04-24 01:43:21 -0400

    Created functionality for all basic user routes.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,55 @@
 const User = require('../models/User');
 
 module.exports = {
+
   getUsers(req, res) {
     User.find()
-      .then((users) => res.json(users))
+      .then((users) => res.status(200).json(users))
       .catch((err) => res.status(500).json(err));
   },
+
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
+      .select('-__v')
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with that ID' })
+          ? res.status(404).json({ message: 'Cannot find user!' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a new user
+
   createUser(req, res) {
     User.create(req.body)
-      .then((dbUserData) => res.json(dbUserData))
+      .then((dbUserData) => res.status(200).json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+
+  updateUser(req, res) {
+    User.findByIdAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { new: true }
+    )
+      .then((updUser => {
+        !updUser
+          ? res.status(404).json({ message: 'Cannot find user!' })
+          : res.status(200).json({ message: 'User has been succesfully updated.'})
+      }
+      ))
+      .catch((err) => res.status(500).json(err))
+  },
+
+  deleteUser(req, res) {
+    User.findByIdAndDelete(
+      { _id: req.params.userId }
+      )
+      .then(
+        !user
+          ? res.status(404).json({ message: 'Cannot find user!' })
+          : res.status(200).json({ message: 'User has been successfully deleted.' })
+      )
+      .catch((err) => res.status(500).json(err))
+  }
+
 };
